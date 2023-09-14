805ab730db42e8a644e52c5ee62df771a112bb12
Yeflix <130116087+Yeflix@users.noreply.github.com>
2023-09-13 20:07:55 -0400

    2da version
    Agregamos un evento al inut

--- script/script.js
+++ script/script.js
@@ -1,13 +1,20 @@
+let botonEnviarDatos= document.getElementById('enviar')
+
+
+
 function desaparecer() {
     let prueba = document.getElementById('prueba')
     prueba.style.display = "none"  
     let resultados = document.getElementById("resultados")
     resultados.style.display = "none"
-    let botonEnviarDatos = document.getElementById('enviar')
-    botonEnviarDatos.addEventListener('click', aparecer)  
+    botonEnviarDatos.addEventListener('click', aparecer)
 }
-
 function aparecer() {
+    let inputNombre = document.getElementById("name2").value
+    alert("bienvenid@ " + inputNombre)
+    alert("Si estas en esta prueba, felicidades!! eso quiere decir que estas capacitado para aprobar esta prueba")
+    alert("Que tengas Mucho Exito y recuerda. El conocimiento es poder")
+    
     let registro = document.getElementById('registro')
     registro.style.display = "none" 
     let prueba = document.getElementById('prueba')
